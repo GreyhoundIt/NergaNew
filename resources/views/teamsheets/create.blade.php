bfa608b945bc5dacb46b232bf3283e2f1b8ac4b4
Matthew <matthew@greyhoundit.co.uk>
2017-02-06 21:17:34 +0000

    styling clubs

--- resources/views/teamsheets/create.blade.php
+++ resources/views/teamsheets/create.blade.php
@@ -20,7 +20,6 @@
                         {{ $club->name }}
 
                     {!! Form::open(array('route' => 'user.team.store')) !!}
-                    <input name="_token" type="hidden" value="{!! csrf_token() !!}" />
                     <div class="row player odd">
                         <div class="col-md-4">
                         {{ Form::label('p1_first_name', 'Player 1 First Name:') }}

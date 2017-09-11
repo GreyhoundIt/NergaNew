482bf5740721fc053ea0c029fedfb415ed57e31e
Matthew <matthew@greyhoundit.co.uk>
2017-09-11 18:20:20 +0100

    finals day

--- resources/views/finals.blade.php
+++ resources/views/finals.blade.php
@@ -11,7 +11,7 @@
                 <p>NERGA will host two separate Finals Days for 2017, with both events taking place at <a href="http://www.woodhamgolfandcountryclub.co.uk/" target="_blank">Woodham Golf and Country Club</a></p>
                 <p>The 2017 Inter Finals day will take place on Saturday 23/9/17 with the Rabbits Finals Day taking place a week later on Saturday 30/9/17.</p>
                 <ul>
-                    <li><a href=https://s3.eu-west-2.amazonaws.com/nerga/finalsday/General+Info+v2.pdf" target="_blank">General Information</a></li>
+                    <li><a href="https://s3.eu-west-2.amazonaws.com/nerga/finalsday/General+Info+v2.pdf" target="_blank">General Information</a></li>
                     <li><a href="https://s3.eu-west-2.amazonaws.com/nerga/finalsday/2017+Finals+Day+Inter+All+++Players.pdf" target="_blank">List of all Inter Players</a></li>
                     <li><a href="https://s3.eu-west-2.amazonaws.com/nerga/finalsday/2017+Finals+Day+Qualifying+Inter+++Players.pdf" target="_blank">List of Qualified Inter players</a></li>
                     <li><a href="https://s3.eu-west-2.amazonaws.com/nerga/finalsday/NERGa+Finals+Day+2017+-+Qualifying++Clubs.pdf">List of Inter qualified clubs</a></li>

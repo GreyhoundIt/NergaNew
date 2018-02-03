c59b937ca55c29124ac8396124e6224e777706da
Matthew <matthew@greyhoundit.co.uk>
2018-02-03 15:28:38 +0000

    typos

--- resources/views/finals.blade.php
+++ resources/views/finals.blade.php
@@ -8,7 +8,7 @@
             <div class="inner-content col-md-12">
                 <h1>Finals Day</h1>
                 <img src="/images/Woodham.jpg" class="center-block leadpic" alt="North East Rabbits League Logo">
-               <p> The 2017 NERGA Finals Day had to be postponed due to inclement weather and has been rearranged for Saturday 31st Match at Ramside Hall.</p>
+               <p> The 2017 NERGA Finals Day had to be postponed due to inclement weather and has been rearranged for Saturday 31st March at Ramside Hall.</p>
                 <p>NERGA will host two separate Finals Days in 2018, with both events taking place at Ramside Hall.
                 The 2018 Inter Finals day will take place on Saturday 6th October with the NERGA Finals Day taking place a week later on Saturday 13th October.</p>
                 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,9 +8,9 @@
             <div class="inner-content col-md-12">
                 <h1>Welcome to the NERGA- The North East Rabbits Golf Association</h1>
                 <img src="/images/pic.jpg" class="leadpic" alt="North East Rabbits League Logo">
-                <p>2018 NERGA (16 to 28 handicap) has 35 Clubs in 4 divided into four zones. Over 550 players will compete as individuals and within their Club team for a place in Finals Day at Ramside Hall in October to become NERGA Club and Individual Champions.</p>
-                <p> In 2015 we launched a mid-handicap league – INTER (8 to 15 handicap). For 2018 there are 27 Clubs divided into three zones; all are competing for a place in the Finals day at Ramside Hall in October</p>
-                <p> If you are a Club interested in joining us 2019 then please contact our Secretary Mike Walke - <a href="mailto:mikewalke88@hotmail.com?subject=NERGA%20WEBSITE">mikewalke88@hotmail.com</a> or 07887 520467 for further information.</p>
+                <p>2018 NERGA (16 to 28 handicap) has 35 Clubs divided into four zones. Over 550 players will compete as individuals and within their Club team for a place in Finals Day at Ramside Hall in October to become NERGA Club and Individual Champions.</p>
+                <p> In 2015 we launched a mid-handicap league – INTER (8 to 15 handicap). For 2018 there are 27 Clubs divided into three zones; all are competing for a place in the Finals day at Ramside Hall in October.</p>
+                <p> If you are a Club interested in joining us in 2019 then please contact our Secretary Mike Walke - <a href="mailto:mikewalke88@hotmail.com?subject=NERGA%20WEBSITE">mikewalke88@hotmail.com</a> or 07887 520467 for further information.</p>
 
             </div>
             <?php echo View::make('partials.footer') ?>

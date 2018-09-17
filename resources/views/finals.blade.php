1055a15cc69206c902350bf2d84354ebbe4b9e54
Matthew <matthew@greyhoundit.co.uk>
2018-09-17 18:19:20 +0100

    finalsday

--- resources/views/finals.blade.php
+++ resources/views/finals.blade.php
@@ -10,9 +10,9 @@
                 <p>NERGA will host two separate Finals Days in 2018, with both events taking place at Ramside Hall.<br/>The 2018 Inter Finals day will take place on Saturday 6th October with the NERGA Finals Day taking place a week later on Saturday 13th October.
                 </p>
                <h3><a href="{{ url ('info/GeneralInfo2018.pdf') }}" target="_blank">Details of the arrangements for both events are included in the attached note.</a> </h3>
-               <h3> <a href="{{ url ('info/FinalsDay2018IntersDraftStartSheet.pdf') }}" target="_blank">The draft start sheet for the Inter Finals Day on 6/10/18s</a></h3>
+               <h3> <a href="{{ url ('info/FinalsDay2018IntersDraftStartSheet.pdf') }}" target="_blank">The draft start sheet for the Inter Finals Day on 6/10/18</a></h3>
                 <h3><a href="{{ url ('info/FinalsDay2018RabbitsDraftStartSheet.pdf') }}" target="_blank">The draft start sheet for the Rabbits Finals Day on 13/10/18</a></h3>
-                <p>The Final start sheet will be available 5 days before the event.</p>
+                <p><b>The Final start sheet will be available 5 days before the event.</b></p>
                 </div>
             <?php echo View::make('partials.footer') ?>
         </div>

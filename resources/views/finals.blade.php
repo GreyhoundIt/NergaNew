5939635c1597d8fc13e15d865f089f1086ac7147
Matthew <matthew@greyhoundit.co.uk>
2018-09-17 18:17:07 +0100

    finalsday

--- resources/views/finals.blade.php
+++ resources/views/finals.blade.php
@@ -7,17 +7,12 @@
             <?php echo View::make('partials.hero') ?>
             <div class="inner-content col-md-12">
                 <h1>Finals Day</h1>
-                <p>The rearranged 2017 Rabbits Finals day was held at the excellent Ramside Hall on Saturday 12/5/18. Full details of the Individual and Team event are included on the attached files.</p>
-               <h3><a href="{{ url ('info/2017-Rabbits-INDIVIDUAL-Result.pdf') }}" target="_blank">Individual Results</a> </h3>
-               <h3> <a href="{{ url ('info/2017-Rabbits-Finals-Day-Team-Result.pdf') }}" target="_blank">Team Results</a></h3>
-                <h3><a href="{{ url ('info/2017-Rabbits-Finals Day-Team-Players-Result.pdf') }}" target="_blank">Team PlayerResults</a></h3>
-
-                <h3><a href="{{ url ('https://www.dropbox.com/sh/2uh11bo6s2u3kgq/AACANyRtXA7M6DLuaLDv1vy5a?dl=0') }}" target="_blank">Photos from the day</a></h3></br>
-
-                <h2>2018</h2>
-                </br>
-                <p>NERGA will host two separate Finals Days in 2018, with both events taking place at Ramside Hall.
-                The 2018 Inter Finals day will take place on Saturday 6th October with the NERGA Finals Day taking place a week later on Saturday 13th October.</p>
+                <p>NERGA will host two separate Finals Days in 2018, with both events taking place at Ramside Hall.<br/>The 2018 Inter Finals day will take place on Saturday 6th October with the NERGA Finals Day taking place a week later on Saturday 13th October.
+                </p>
+               <h3><a href="{{ url ('info/GeneralInfo2018.pdf') }}" target="_blank">Details of the arrangements for both events are included in the attached note.</a> </h3>
+               <h3> <a href="{{ url ('info/FinalsDay2018IntersDraftStartSheet.pdf') }}" target="_blank">The draft start sheet for the Inter Finals Day on 6/10/18s</a></h3>
+                <h3><a href="{{ url ('info/FinalsDay2018RabbitsDraftStartSheet.pdf') }}" target="_blank">The draft start sheet for the Rabbits Finals Day on 13/10/18</a></h3>
+                <p>The Final start sheet will be available 5 days before the event.</p>
                 </div>
             <?php echo View::make('partials.footer') ?>
         </div>
